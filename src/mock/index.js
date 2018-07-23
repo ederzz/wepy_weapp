177481da94e127bb76f4c2b82f8c934ed9c11754
shenyiling <jzx19951028yyjy@163.com>
2018-07-23 10:23:25 +0800

    feat: mockjs模拟数据

---
+++ src/mock/index.js
@@ -0,0 +1,33 @@
+import Mock from 'mockjs'
+
+const goodsData = Mock.mock({
+  "hotGood|10-20": [
+    {
+      "img_url|1": [
+        "http://img.hb.aicdn.com/3386bf54024cb403acad0e0f32403320d3d56fe196ff7-KhnGDO_fw658",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322030346&di=dfbbe7ba91874f3b7c6bc481f15c2b36&imgtype=0&src=http%3A%2F%2Fimg010.hc360.cn%2Fk1%2FM00%2FA9%2F9F%2FwKhQw1eq6wKEffK4AAAAALD6HAk873.jpg",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322008618&di=0f5c5a0092043786419dddf72f5bc6d4&imgtype=0&src=http%3A%2F%2Fpic.ds365.com%2FUpload%2FShop%2FImages%2FProduct%2F20151103%2F201511031403037743411.JPG"
+      ],
+      "name|1": [
+        "卫龙亲嘴烧",
+        "菊乐酸牛奶",
+        "伊利纯牛奶",
+        "奥利奥原味夹心饼干",
+        "红牛250ml",
+        "可口可乐汽水330ml"
+      ],
+      "price|1": [
+        3.00,
+        6.20,
+        0.50,
+        2.50,
+        3.30,
+        3.70
+      ],
+    }
+  ] 
+})
+
+export {
+  goodsData
+}
--- src/store/reducers/goods.js
+++ src/store/reducers/goods.js
@@ -1,10 +1,6 @@
 import { handleActions } from 'redux-actions'
 import { GET_HOT_GOODS } from '../types/goods'
+import { goodsData } from '../../mock'
+console.log(goodsData, '======')
 
-export default handleActions({
-  [GET_HOT_GOODS] (state) {
-    return {}
-  }
-}, {
-
-})
+export default handleActions({ }, goodsData)
