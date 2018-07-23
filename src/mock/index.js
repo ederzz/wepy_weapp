62fd6391fe563338bed47ccd7e8b0341ebbd5752
shenyiling <jzx19951028yyjy@163.com>
2018-07-23 16:43:41 +0800

    fix: 循环渲染组件bug

--- src/mock/index.js
+++ src/mock/index.js
@@ -1,7 +1,7 @@
 import Mock from 'mockjs'
 
 const goodsData = Mock.mock({
-  "hotGood|10-20": [
+  "hotGoods|10-20": [
     {
       "img_url|1": [
         "http://img.hb.aicdn.com/3386bf54024cb403acad0e0f32403320d3d56fe196ff7-KhnGDO_fw658",
@@ -25,7 +25,82 @@ const goodsData = Mock.mock({
         3.70
       ],
     }
-  ] 
+  ],
+  "defaultShelves|10-20": [
+    {
+      "img_url|1": [
+        "http://img.hb.aicdn.com/3386bf54024cb403acad0e0f32403320d3d56fe196ff7-KhnGDO_fw658",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322030346&di=dfbbe7ba91874f3b7c6bc481f15c2b36&imgtype=0&src=http%3A%2F%2Fimg010.hc360.cn%2Fk1%2FM00%2FA9%2F9F%2FwKhQw1eq6wKEffK4AAAAALD6HAk873.jpg",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322008618&di=0f5c5a0092043786419dddf72f5bc6d4&imgtype=0&src=http%3A%2F%2Fpic.ds365.com%2FUpload%2FShop%2FImages%2FProduct%2F20151103%2F201511031403037743411.JPG"
+      ],
+      "name|1": [
+        "蒙牛真果粒",
+        "椰树椰汁450ml",
+        "达能脉动600ml",
+        "奥利奥原味夹心饼干",
+        "美好泡椒热狗肠",
+        "可口可乐汽水330ml"
+      ],
+      "price|1": [
+        3.00,
+        6.20,
+        0.50,
+        2.50,
+        3.30,
+        3.70
+      ],
+    }
+  ] ,
+  "snacks|10-20": [
+    {
+      "img_url|1": [
+        "http://img.hb.aicdn.com/3386bf54024cb403acad0e0f32403320d3d56fe196ff7-KhnGDO_fw658",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322030346&di=dfbbe7ba91874f3b7c6bc481f15c2b36&imgtype=0&src=http%3A%2F%2Fimg010.hc360.cn%2Fk1%2FM00%2FA9%2F9F%2FwKhQw1eq6wKEffK4AAAAALD6HAk873.jpg",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322008618&di=0f5c5a0092043786419dddf72f5bc6d4&imgtype=0&src=http%3A%2F%2Fpic.ds365.com%2FUpload%2FShop%2FImages%2FProduct%2F20151103%2F201511031403037743411.JPG"
+      ],
+      "name|1": [
+        "旺旺小小酥",
+        "农心鲜虾条",
+        "旺旺煎豆",
+        "大白兔原味奶糖",
+        "红牛250ml",
+        "可口可乐汽水330ml"
+      ],
+      "price|1": [
+        3.00,
+        6.20,
+        0.50,
+        2.50,
+        3.30,
+        3.70
+      ],
+    }
+  ] ,
+  "fastFood|10-20": [
+    {
+      "img_url|1": [
+        "http://img.hb.aicdn.com/3386bf54024cb403acad0e0f32403320d3d56fe196ff7-KhnGDO_fw658",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322030346&di=dfbbe7ba91874f3b7c6bc481f15c2b36&imgtype=0&src=http%3A%2F%2Fimg010.hc360.cn%2Fk1%2FM00%2FA9%2F9F%2FwKhQw1eq6wKEffK4AAAAALD6HAk873.jpg",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1532322008618&di=0f5c5a0092043786419dddf72f5bc6d4&imgtype=0&src=http%3A%2F%2Fpic.ds365.com%2FUpload%2FShop%2FImages%2FProduct%2F20151103%2F201511031403037743411.JPG"
+      ],
+      "name|1": [
+        "旺旺仙贝",
+        "统一牛肉面",
+        "旺旺大米饼52g",
+        "康师傅红烧牛肉面",
+        "统一荞面馆麻辣笋子牛肉方便面112g",
+        "卫龙大面筋106g"
+      ],
+      "price|1": [
+        3.00,
+        6.20,
+        4.50,
+        2.50,
+        1.30,
+        3.70
+      ],
+    }
+  ]  
 })
 
 export {
