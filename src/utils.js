2a4dc06c9aab4f0fd04c127c1214f69bcd74ef98
shenyiling <jzx19951028yyjy@163.com>
2018-11-22 23:44:35 +0800

    fix: modify getRandomHsl function in utils.js file

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 /**
  * 随机HSL颜色值
  */
-export const getRandomHSL = `hsl(${360 * Math.random()}, ${25 + 65 * Math.random()}%, ${65 + 25 * Math.random()}%)`
+export const getRandomHSL = () => `hsl(${360 * Math.random()}, ${25 + 65 * Math.random()}%, ${65 + 25 * Math.random()}%)`
